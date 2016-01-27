7b4a2e94b55a36023ed05128d1863678eb0408b5
Kilbourne <luca.castellone@gmail.com>
2016-01-27 14:30:08 +0100

    first commit

---
+++ web/app/themes/faberge/front-page.php
@@ -0,0 +1,46 @@
+<div class="row">
+  <div class="col">
+    <div class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/template_03.jpg" alt=""></div>
+    <div class="claim" style="left: 1.67%; bottom: 3%; width: 44%;" >
+      <h5 style="font-weight: 400;">Eggs</h5>
+      <p>All the pieces have remarkable history and are made with highest level of details and quality.</p>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col u-6/11 inline-block" >
+    <div class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/template_05.jpg" alt=""></div>
+    <div class="claim" style="bottom: 1%; right: 4%; width:20%;"><h5>TABLE EGGS</h5>
+    <p>collection</p></div>
+  </div><div class="col u-5/11  inline-block">
+    <div class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/template_09.jpg" alt=""></div>
+    <div class="claim" style="top: 2%; right: 4%;"><h5>HERMITAGE</h5>
+    <p>collection</p></div>
+  </div>
+</div>
+<div class="row">
+  <div class="col u-5/11 inline-block">
+    <div class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/template_15.jpg" alt=""></div>
+    <div class="claim" style="top: 15%; left: 3.67%; width: 40%;">
+    <p>Luxury accessories for women</p></div>
+  </div><div class="col u-6/11  inline-block">
+    <div class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/template_13.jpg" alt=""></div>
+        <div class="claim" style="top: 7%; width: 26%; left: 33%;color:#000;">
+    <p>A love history, collaboration and new beginnings</p></div>
+  </div>
+</div>
+<div class="row">
+  <div class="col u-5/11 inline-block">
+    <div class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/template_16.jpg" alt=""></div>
+        <div class="claim">
+    <p>Luxury accessories for man</p></div>
+  </div><div class="col u-6/11 inline-block">
+    <div class="row">
+      <div class="col history-box">
+        <div class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/template_.jpg" alt=""></div>
+      </div><div class="col locator-box">
+        <div class="img-wrap"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/template_.jpg" alt=""></div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ web/app/themes/faberge/templates/header.php
@@ -0,0 +1,10 @@
+<header class="banner">
+  <div class="container">
+
+    <nav class="nav-primary">
+      <li></li>
+      <li class="logo" ><div><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/logo.png" alt=""></div></li>
+      <li></li>
+    </nav>
+  </div>
+</header>
