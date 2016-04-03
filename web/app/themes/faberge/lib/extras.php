b68d4fb3376ed536052d6af65f4ed50cc1ae23cc
Kilbourne <luca.castellone@gmail.com>
2016-04-03 23:42:06 +0200

    Extras

--- web/app/themes/faberge/lib/extras.php
+++ web/app/themes/faberge/lib/extras.php
@@ -18,7 +18,7 @@ function body_class($classes) {
   if(is_product_category()){
     global $wp_query;
       $cat = $wp_query->get_queried_object();
-      
+
       $parent=$cat->parent;
       if($parent !== 0){ $classes[]= 'single-product';}
   }
@@ -39,34 +39,9 @@ function excerpt_more() {
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
 
-
-function faberge_get_product_detail() {
-  $product_id = $_POST['product_id'];
-  $args = array(
-    'post_type' => 'product',
-    'p'  => $product_id,
-  );
-  $query = new WP_Query( $args );
-  if(!wp_script_is( 'wc-add-to-cart-variation' )){ wp_enqueue_script( 'wc-add-to-cart-variation' ); }
-
-  if ($query->have_posts()) :
-        while ($query->have_posts()) : $query->the_post();
-            wc_get_template_part( 'content', 'single-product' );
-        endwhile;
-     else :
-        echo "There were no posts found";
-    endif;
-    wp_reset_query();
-  //faberge_woocommerce_single_product_variations(FALSE);
-  wp_die();
-}
-add_action('wp_ajax_get_product_detail', __NAMESPACE__ . '\\faberge_get_product_detail');
-add_action('wp_ajax_nopriv_get_product_detail', __NAMESPACE__ . '\\faberge_get_product_detail');
-
-
 add_filter('wp_nav_menu_items', __NAMESPACE__ . '\\add_biogena_logo_menu', 10, 2);
 function add_biogena_logo_menu( $items, $args ){
-      
+
     if( $args->menu == 'menu-1' ) // only for primary menu
     {
       $new_item       = array( '<li class="divisor menu-item menu-item-type-post_type menu-item-object-page"><hr/></li>' );
