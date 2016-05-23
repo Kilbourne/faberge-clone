8b212da8358c9687147194c7739aee1e5019c7d8
Kilbourne <luca.castellone@gmail.com>
2016-05-23 14:04:31 +0200

    Footer Additional info

--- web/app/themes/faberge/templates/footer.php
+++ web/app/themes/faberge/templates/footer.php
@@ -11,9 +11,9 @@
     <div class="credits">
       <div class="left">
       <span>©2016</span>
-      <span>MAISON TATIANA FABERGE</span>
+      <span>MAISON TATIANA FABERGE SA</span>
       <span>GENEVE</span>
-      <span>Fondée en 1974</span>    </div>
+      <span>Fondée en 1974</span><span>|  Geneva, Switzerland</span><span>|  CH-660.0.115.974-8</span>    </div>
       <div class="right"><span>web agency</span>
       <span><a href="http://www.menthalia.com"><img src="<?php echo get_stylesheet_directory_uri().'/dist/images/logo-menthalia.png' ?>" alt="MENTHALIA LOGO"></a> </span> </div>
     </div>
