ab9d73d09b562e5fff1eea6b7ee4283ee557d68b
Kilbourne <luca.castellone@gmail.com>
2016-05-30 10:01:31 +0200

    analytics verification

--- web/app/themes/faberge/templates/head.php
+++ web/app/themes/faberge/templates/head.php
@@ -4,5 +4,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href='https://fonts.googleapis.com/css?family=Antic+Didone' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+  <meta name="google-site-verification" content="viVQZ9CEyfXaZIirZjmwtcqpO95wKseqT0vJSiyvlPY" />
   <?php wp_head(); ?>
 </head>
