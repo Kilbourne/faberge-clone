d18fb2d0025cdbc34fc7c551223ef73e276461a0
Kilbourne <luca.castellone@gmail.com>
2016-07-01 00:40:33 +0200

    Move font links

--- web/app/themes/faberge/templates/head.php
+++ web/app/themes/faberge/templates/head.php
@@ -2,8 +2,9 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href='https://fonts.googleapis.com/css?family=Antic+Didone' rel='stylesheet' type='text/css'>
-  <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
   <meta name="google-site-verification" content="viVQZ9CEyfXaZIirZjmwtcqpO95wKseqT0vJSiyvlPY" />
   <?php wp_head(); ?>
+  <link href='https://fonts.googleapis.com/css?family=Antic+Didone' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+
 </head>
