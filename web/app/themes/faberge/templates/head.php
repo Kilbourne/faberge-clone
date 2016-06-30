fd3e93344008c37748644837b7f573ee8996c79f
Kilbourne <luca.castellone@gmail.com>
2016-07-01 01:25:23 +0200

    Allow file modification

--- web/app/themes/faberge/templates/head.php
+++ web/app/themes/faberge/templates/head.php
@@ -2,7 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="google-site-verification" content="viVQZ9CEyfXaZIirZjmwtcqpO95wKseqT0vJSiyvlPY" />
+    <meta name="google-site-verification" content="viVQZ9CEyfXaZIirZjmwtcqpO95wKseqT0vJSiyvlPY" />
   <?php wp_head(); ?>
   <link href='https://fonts.googleapis.com/css?family=Antic+Didone' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
