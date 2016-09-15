19a0a0f9815abd5c01ad99aca5931010f59df3bb
Kilbourne <luca.castellone@gmail.com>
2016-09-15 20:05:15 +0200

    Woocommerce  Template

--- web/app/themes/faberge/woocommerce/cart/mini-cart.php
+++ web/app/themes/faberge/woocommerce/cart/mini-cart.php
@@ -36,7 +36,63 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 				if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_widget_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
 
-					$product_name  = apply_filters( 'woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key );
+      $item_data = array();
+
+      // Variation data
+      if ( ! empty( $cart_item['data']->variation_id ) && is_array( $cart_item['variation'] ) ) {
+
+        foreach ( $cart_item['variation'] as $name => $value ) {
+
+          if ( '' === $value )
+            continue;
+
+          $taxonomy = wc_attribute_taxonomy_name( str_replace( 'attribute_pa_', '', urldecode( $name ) ) );
+
+          // If this is a term slug, get the term's nice name
+          if ( taxonomy_exists( $taxonomy ) ) {
+            $term = get_term_by( 'slug', $value, $taxonomy );
+            if ( ! is_wp_error( $term ) && $term && $term->name ) {
+              $value = $term->name;
+            }
+            $label = wc_attribute_label( $taxonomy );
+
+          // If this is a custom option slug, get the options name
+          } else {
+            $value              = apply_filters( 'woocommerce_variation_option_name', $value );
+            $product_attributes = $cart_item['data']->get_attributes();
+            if ( isset( $product_attributes[ str_replace( 'attribute_', '', $name ) ] ) ) {
+              $label = wc_attribute_label( $product_attributes[ str_replace( 'attribute_', '', $name ) ]['name'] );
+            } else {
+              $label = $name;
+            }
+          }
+
+          $item_data[] = array(
+            'key'   => $label,
+            'value' => $value
+          );
+        }
+        $product_name  = apply_filters( 'woocommerce_cart_item_name', $_product->get_sku(), $cart_item, $cart_item_key );
+
+      }else{
+      	$product_name  = apply_filters( 'woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key );
+      }
+
+      // Filter item data to allow 3rd parties to add more to the array
+      $item_data = apply_filters( 'woocommerce_get_item_data', $item_data, $cart_item );
+
+      // Format item data ready to display
+      foreach ( $item_data as $key => $data ) {
+        // Set hidden to true to not display meta on cart.
+        if ( ! empty( $data['hidden'] ) ) {
+          unset( $item_data[ $key ] );
+          continue;
+        }
+        $item_data[ $key ]['key']     = ! empty( $data['key'] ) ? $data['key'] : $data['name'];
+        $item_data[ $key ]['display'] = ! empty( $data['display'] ) ? $data['display'] : $data['value'];
+      }
+
+					
 					$thumbnail     = apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key );
 					$product_price = apply_filters( 'woocommerce_cart_item_price', WC()->cart->get_product_price( $_product ), $cart_item, $cart_item_key );
 					?>
@@ -51,13 +107,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 						), $cart_item_key );
 						?>
 						<?php if ( ! $_product->is_visible() ) : ?>
-							<?php echo str_replace( array( 'http:', 'https:' ), '', $thumbnail ) . $product_name . '&nbsp;'; ?>
-						<?php else : ?>
-							<a href="<?php echo esc_url( $_product->get_permalink( $cart_item ) ); ?>">
-								<?php echo str_replace( array( 'http:', 'https:' ), '', $thumbnail ) . $product_name . '&nbsp;'; ?>
-							</a>
-						<?php endif; ?>
-						<?php echo WC()->cart->get_item_data( $cart_item ); ?>
+	<?php echo str_replace( array( 'http:', 'https:' ), '', $thumbnail ) ; ?>
+<?php else : ?>
+	<a href="<?php echo esc_url( $_product->get_permalink( $cart_item ) ); ?>">
+		<?php echo str_replace( array( 'http:', 'https:' ), '', $thumbnail )  ; ?>
+	</a>
+<?php endif; ?>
+<?php if ( ! $_product->is_visible() ) : ?>
+	<?php echo  $product_name ; ?>
+<?php else : ?>
+	<a href="<?php echo esc_url( $_product->get_permalink( $cart_item ) ); ?>">
+		<?php echo $product_name ; ?>
+	</a>
+<?php endif; ?>
+	
+						<?php //echo WC()->cart->get_item_data( $cart_item ); ?>
 
 						<?php echo apply_filters( 'woocommerce_widget_cart_item_quantity', '<span class="quantity">' . sprintf( '%s &times; %s', $cart_item['quantity'], $product_price ) . '</span>', $cart_item, $cart_item_key ); ?>
 					</li>
