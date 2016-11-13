864d327e9856d9930cb9e5c17504d7c8e9103278
Kilbourne <luca.castellone@gmail.com>
2016-11-14 00:19:22 +0100

    Performance remove price form woocommerce archive loop

--- web/app/themes/faberge/lib/woocommerce.php
+++ web/app/themes/faberge/lib/woocommerce.php
@@ -17,6 +17,8 @@ remove_action( 'woocommerce_before_shop_loop','woocommerce_catalog_ordering', 30
 remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
 remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
 remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+
 /**
  * Archive Description
  *
